32c9b344a3b0969eb8a9c38d843bcb676c45af1a
Laboratorio <laboratorio@udla-ec.int>
2024-01-08 16:38:34 -0500

    Ejercicio de funcion con punteros

--- claselunes.c
+++ claselunes.c
@@ -1,8 +1,18 @@
-#include<stdio.h>
-#include<stdlib.h>
-int sumar(int a, int b){
+#include <stdio.h>
+#include <stdlib.h>
+int sumar(int a, int b)
+{
     return a + b;
 }
+// Le pongo void porque en los mismos parametros voy a usar el resultado
+// & direccionamiento - sobre la direccion
+// * indireccionamiento - sobre el contenido
+void sumarPorReferencia(int *a, int *b, int *resultado){ // en este
+ *resultado = *a + *b;
+ printf("El resultado de la suma es: %d", *resultado);
+
+}
+
 int main()
 {
     int num1, num2, suma = 0;
@@ -13,8 +23,9 @@ int main()
 
     suma = sumar(num1, num2);
     printf("El valor de la suma es: %d", suma);
-    
+
+    printf("\n\n");
+    sumarPorReferencia(&num1, &num2, &suma);
+
     return 0;
 }
-
-
